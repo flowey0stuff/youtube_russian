0137092407385061865a260f9985f12a1990b3bf
flowey0stuff <ridbon.net@gmail.com>
2024-08-19 15:43:04 +0500

    рероллит прокси

--- main.py
+++ main.py
@@ -26,87 +26,102 @@ def check_proxy(proxy_url):
         response = requests.get("http://www.google.com", proxies=proxies, timeout=10, verify=False)
         if response.status_code == 200:
             print("Прокси работает")
+            return True
         else:
             print("Прокси не работает, статус:", response.status_code)
-            sys.exit()
+            return False
     except Exception as e:
         print("Ошибка подключения к прокси:", str(e))
-        sys.exit()
+        return False
+
+def get_working_proxy():
+    proxy_list = get_proxy_list()
+    while True:
+        proxy_url = get_random_proxy(proxy_list)
+        print(f"Проверка прокси-сервера: {proxy_url}")
+        if check_proxy(proxy_url):
+            return proxy_url
 
 def sanitize_filename(filename):
     """Удаляет недопустимые символы из имени файла для Windows"""
     return re.sub(r'[<>:"/\|?*]', '', filename)
 
 def download_and_play_youtube_video(search_term, by_url, advanced_search=False):
-    proxy_list = get_proxy_list()
-    proxy_url = get_random_proxy(proxy_list)  # Получаем случайный прокси URL
+    proxy_url = get_working_proxy()  # Получаем рабочий прокси URL
     print(f"Используемый прокси-сервер: {proxy_url}")
-    check_proxy(proxy_url)  # Проверяем прокси
-    ydl_opts = {
+
+    ydl_opts_no_proxy = {
         'format': 'best',
         'outtmpl': 'downloaded_video.%(ext)s',  # Сохраняем видео в текущей директории
-        'proxy': proxy_url,
-        'nocheckcertificate': True,  # Отключение проверки сертификатов
     }
+    ydl_opts_with_proxy = {
+        'format': 'best',
+        'outtmpl': 'downloaded_video.%(ext)s',
+        'proxy': proxy_url,
+        'nocheckcertificate': True
+    }
+
     video_url = None
     video_title = None
 
     if advanced_search:
         query = f"ytsearch10:{search_term}"
-        with yt_dlp.YoutubeDL({'quiet': True, 'proxy': proxy_url}) as ydl:
-            try:
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts_no_proxy) as ydl:
                 result = ydl.extract_info(query, download=False)
-            except yt_dlp.utils.DownloadError as e:
-                print(f"Ошибка при разборе JSON: {str(e)}")
-                return
+        except yt_dlp.utils.DownloadError as e:
+            print(f"Ошибка при разборе JSON: {str(e)}")
+            return
 
-            if result and 'entries' in result:
-                while True:
-                    print("Введите цифру для выбора видео или 11 для загрузки ещё 10 видео:")
-                    for idx, entry in enumerate(result['entries'], start=1):
-                        print(f"{idx}) {entry['title']} | {entry['uploader']} | {entry['duration'] // 60}мин {entry['duration'] % 60}сек")
+        if result and 'entries' in result:
+            while True:
+                print("Введите цифру для выбора видео или 11 для загрузки ещё 10 видео:")
+                for idx, entry in enumerate(result['entries'], start=1):
+                    print(f"{idx}) {entry['title']} | {entry['uploader']} | {entry['duration'] // 60}мин {entry['duration'] % 60}сек")
 
-                    print("11) Загрузить остальные 10 видео из поиска")
-                    choice = input("Ваш выбор: ")
-                    if choice.isdigit() and int(choice) in range(1, 12):
-                        choice = int(choice)
-                        if choice == 11:
-                            if 'continuation' in result:
-                                result = ydl.extract_info(f"ytsearch10:{search_term}&sp={result['continuation']}", download=False)
-                            else:
-                                print("Больше нет видео для загрузки.")
-                                return
+                print("11) Загрузить остальные 10 видео из поиска")
+                choice = input("Ваш выбор: ")
+                if choice.isdigit() and int(choice) in range(1, 12):
+                    choice = int(choice)
+                    if choice == 11:
+                        if 'continuation' in result:
+                            result = ydl.extract_info(f"ytsearch10:{search_term}&sp={result['continuation']}", download=False)
                         else:
-                            video_url = result['entries'][choice - 1].get('url')
-                            video_title = result['entries'][choice - 1].get('title')
-                            break
+                            print("Больше нет видео для загрузки.")
+                            return
                     else:
-                        print("Неверный выбор. Попробуйте снова.")
-            else:
-                print("Видео не найдено по запросу. Попробуйте снова.")
-                return
+                        video_url = result['entries'][choice - 1].get('url')
+                        video_title = result['entries'][choice - 1].get('title')
+                        break
+                else:
+                    print("Неверный выбор. Попробуйте снова.")
+        else:
+            print("Видео не найдено по запросу. Попробуйте снова.")
+            return
     else:
         if by_url:
             video_url = search_term
         else:
             query = f"ytsearch:{search_term}"
-            with yt_dlp.YoutubeDL({'quiet': True, 'proxy': proxy_url}) as ydl:
-                try:
+            try:
+                with yt_dlp.YoutubeDL(ydl_opts_no_proxy) as ydl:
                     result = ydl.extract_info(query, download=False)
-                except yt_dlp.utils.DownloadError as e:
-                    print(f"Ошибка при разборе JSON: {str(e)}")
-                    return
-                if result and 'entries' in result:
-                    entry = result['entries'][0] if result['entries'] else {}
-                    video_url = entry.get('url')
-                    video_title = entry.get('title')
-                if not video_url or not video_title:
-                    print("Видео не найдено по запросу. Попробуйте снова.")
-                    return
+                    if result and 'entries' in result:
+                        entry = result['entries'][0] if result['entries'] else {}
+                        video_url = entry.get('url')
+                        video_title = entry.get('title')
+            except yt_dlp.utils.DownloadError as e:
+                print(f"Ошибка при разборе JSON: {str(e)}")
+                return
 
-    # Использование yt-dlp для загрузки видео
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([video_url])
+        if not video_url or not video_title:
+            print("Видео не найдено по запросу. Попробуйте снова.")
+            return
+
+    # Использование yt-dlp с прокси для загрузки видео
+    if video_url:
+        with yt_dlp.YoutubeDL(ydl_opts_with_proxy) as ydl:
+            ydl.download([video_url])
 
     # Поиск имени загруженного файла
     downloaded_file = None
@@ -140,10 +155,10 @@ def main():
     print("Напишите 1 чтобы найти видео с помощью поиска")
     print("Напишите 2 чтобы найти видео с помощью URL")
     print("Напишите 3 чтобы использовать продвинутый поиск (рекомендуется)")
-    []
     choice = input("Ваш выбор: ")
 
     if choice == '1':
+        []
         search_term = input("Введите название видео для поиска: ")
         download_and_play_youtube_video(search_term, by_url=False)
     elif choice == '2':
